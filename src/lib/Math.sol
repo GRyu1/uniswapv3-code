89a4da29b7628330d43d7214582caa0ca4bd76d5
GRyu1 <heliotrop1024@gmail.com>
2024-02-20 22:52:27 +0900

    swap complete

--- src/lib/Math.sol
+++ src/lib/Math.sol
@@ -10,18 +10,90 @@ library Math {
         uint128 liquidity
     ) internal pure returns (uint256 amount0) {
         if (sqrtPriceAX96 > sqrtPriceBX96)
-        (sqrtPriceAX96, sqrtPriceBX96) = (sqrtPriceBX96, sqrtPriceAX96);
+            (sqrtPriceAX96, sqrtPriceBX96) = (sqrtPriceBX96, sqrtPriceAX96);
 
         require(sqrtPriceAX96 > 0);
 
-          amount0 = divRoundingUp(
-        mulDivRoundingUp(
-            (uint256(liquidity) << FixedPoint96.RESOLUTION),
-            (sqrtPriceBX96 - sqrtPriceAX96),
-            sqrtPriceBX96
-        ),
-        sqrtPriceAX96
-    );
+        amount0 = divRoundingUp(
+            mulDivRoundingUp(
+                (uint256(liquidity) << FixedPoint96.RESOLUTION),
+                (sqrtPriceBX96 - sqrtPriceAX96),
+                sqrtPriceBX96
+            ),
+            sqrtPriceAX96
+        );
     }
-    
+
+    function calcAmount1Delta(
+        uint160 sqrtPriceAX96,
+        uint160 sqrtPriceBX96,
+        uint128 liquidity
+    ) internal pure returns (uint256 amount1) {
+        if (sqrtPriceAX96 > sqrtPriceBX96)
+            (sqrtPriceAX96, sqrtPriceBX96) = (sqrtPriceBX96, sqrtPriceAX96);
+
+        amount1 = mulDivRoundingUp(
+            liquidity,
+            (sqrtPriceBX96 - sqrtPriceAX96),
+            FiexedPoint96.Q96
+        );
+    }
+
+    function getNextSqrtPriceFromInput(
+        uint160 sqrtPriceX96,
+        uint128 liquiidity,
+        uint256 amountIn,
+        bool zeroForOne
+    ) internal pure returns (uint160 sqrtPriceNextX96) {
+        sqrtPriceNextX96 = zeroForOne
+            ? (
+                getNextSqrtPriceFromAmount0RoundingUp(
+                    sqrtPriceX96,
+                    liquidity,
+                    amountIn
+                )
+            )
+            : (
+                getNextSqrtPriceFromAmount1RoundingUp(
+                    sqrtPriceX96,
+                    liquidity,
+                    amountIn
+                )
+            );
+    }
+
+    function getNextSqrtPriceFromAmount0RoundingUp(
+        uint160 sqrtPriceX96,
+        uint128 liquidity,
+        uint256 amountIn
+    ) internal pure returns (uint160) {
+        uint256 numerator = uint256(liquidity) << FixedPoint96.RESOLUTION;
+        uint256 product = amountIn * sqrtPriceX96;
+
+        if (product / amountIn == sqrtPriceX96) {
+            uint256 denominator = numerator + product;
+            if (denominator >= numerator) {
+                return
+                    uint160(
+                        mulDivRoundingUp(numerator, sqrtPriceX96, denominator)
+                    );
+            }
+        }
+
+        return
+            uint160(
+                divRoundingUp(numerator, (numerator / sqrtPriceX96) + amountIn)
+            );
+    }
+
+    function getNextSqrtPriceFromAmount1RoundingUp(
+        uint160 sqrtPriceX96,
+        uint128 liquidity,
+        uint256 amountIn
+    ) internal pure returns (uint160) {
+        return
+            uint160(
+                sqrtPriceX96 + uint160((amountIn << FixedPoint96.RESOLUTION) / liquidity)
+            );
+    }
 }
